97b0acb5fd4512286706855c12868248d95cee79
Patrick Shechet <patrick.shechet@cta.com>
2021-01-24 17:02:30 -0700

    example tweaks

--- README.md
+++ README.md
@@ -64,7 +64,11 @@
     <li>
       <a href="#getting-started">Getting Started</a>
     </li>
-    <li><a href="#usage">Usage</a></li>
+    <li><a href="#usage">Usage</a>
+      <ul>
+        <li><a href="#further-examples">Further Examples</a></li>
+      </ul>
+    </li>
     <li><a href="#roadmap">Roadmap</a></li>
     <li><a href="#contributing">Contributing</a></li>
     <li><a href="#license">License</a></li>
--- examples/Statements.py
+++ examples/Statements.py
@@ -15,14 +15,25 @@ stmt[Statement.Order.SELECT] = "SELECT i, r"
 stmt[Statement.Order.FROM] = "FROM random_data"
 stmt[Statement.Order.CTE] = """WITH RECURSIVE random_data AS (
     SELECT 1 AS i, Random() AS r
-    UNION ALL
+UNION ALL
     SELECT i+1 as i, Random() as r
     FROM random_data
-    WHERE i <= 15
+    WHERE i <= 5
 ) """
 
 print(stmt.sql())
+# WITH RECURSIVE random_data AS (
+#     SELECT 1 AS i, Random() AS r
+# UNION ALL
+#     SELECT i+1 as i, Random() as r
+#     FROM random_data
+#     WHERE i <= 5
+# )
+# SELECT i, r
+# FROM random_data
 
 with Cursor(":memory:") as cur:
     r = cur.execute(stmt) # dsORM.Cursor.execute accepts Statements
+    
 print(r)
+# [{'i': 1, 'r': 1948344763645089057}, {'i': 2, 'r': 6157987235773962892}...
